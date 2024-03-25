89cfe889c90aba1f7bbbe524b1ad38101b3f7a1a
sedep040508 <elikbaevasedeppka@gmail.com>
2024-03-25 10:18:48 +0600

    added footer

--- core.js
+++ core.js
@@ -29,27 +29,7 @@ function addCards(el) {
 <path d="M2 9.1371C2 14 6.01943 16.5914 8.96173 18.9109C10 19.7294 11 20.5 12 20.5C13 20.5 14 19.7294 15.0383 18.9109C17.9806 16.5914 22 14 22 9.1371C22 4.27416 16.4998 0.825464 12 5.50063C7.50016 0.825464 2 4.27416 2 9.1371Z" fill="#1C274C" class="qwer" id="btn"
 />
 </svg>
-      <a href="#openModal"><img src=${el.imges_2} alt=""></a>
-      <div id="openModal" class="modal">
-            <div class="modal-dialog">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <h3 class="modal-title">Связаться с пользователем</h3>
-                  <a href="#close" title="Close" class="close">×</a>
-                </div>
-                <div class="modal-body">    
-                <form>
-      <label for="message">Сообщение:</label><br>
-      <textarea id="message" name="message" rows="4" cols="50" class="textarea"></textarea><br>
-      <button type="submit" class="submit-button">Отправить</button>
-    </form>
-                </div>
-              </div>
-            </div>
-          </div>
-    
-  </div>
-    </div>
+<a href="#"><img src=${el.imges_2} alt=""></a>
     
   `
   cardsContainer.insertAdjacentHTML('beforeend', card)
@@ -187,71 +167,5 @@ $('.close-popup').click(function() {
 });
 
 
-var accordeonButtons = document.getElementsByClassName("accordeon__button");
 
-//пишем событие при клике на кнопки - вызов функции toggle
-for(var i = 0; i < accordeonButtons.length; i++) {
-    var accordeonButton = accordeonButtons[i];
 
-    accordeonButton.addEventListener("click", toggleItems, false);
-}
-
-//пишем функцию
-function toggleItems() {
-
-    // переменная кнопки(актульная) с классом
-    var itemClass = this.className;
-
-    // добавляем всем кнопкам класс close
-    for(var i = 0; i < accordeonButtons.length; i++) {
-        accordeonButtons[i].className = "accordeon__button closed";
-    }
-
-    // закрываем все открытые панели с текстом
-    var pannels = document.getElementsByClassName("accordeon__panel");
-    for (var z = 0; z < pannels.length; z++) {
-        pannels[z].style.maxHeight = 0;
-    }
-
-    // проверка. если кнопка имеет класс close при нажатии
-    // к актуальной(нажатой) кнопке добававляем активный класс
-    // а панели - которая находится рядом задаем высоту
-    if(itemClass == "accordeon__button closed") {
-        this.className = "accordeon__button active";
-        var panel = this.nextElementSibling;
-        panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-
-}
-
-(function () {
-
-  let counter = document.querySelectorAll('.counter');
-  let limit = 0; // Переменная, чтобы останавливать функцию, когда всё запустится.
-  window.addEventListener('scroll', function () {
-    if (limit == counter.length) {
-      return;
-    }
-
-    for (let i = 0; i < counter.length; i++) {
-      let pos = counter[i].getBoundingClientRect().top; //Позиция блока, считая сверху окна
-      let win = window.innerHeight - 40; // На 40 пикселей меньше, чем высота окна
-      if (pos < win && counter[i].dataset.stop === "0") {
-        counter[i].dataset.stop = 1; // Останавливаем перезапуск счета в этом блоке
-        let x = 0;
-        limit++; // Счетчик будет запущен, увеличиваем переменную на 1
-        let int = setInterval(function () {
-          // Раз в 60 миллисекунд будет прибавляться 50-я часть нужного числа
-          x = x + Math.ceil(counter[i].dataset.to / 20);
-          counter[i].innerText = x;
-          if (x > counter[i].dataset.to) {
-            //Как только досчитали - стираем интервал.
-            counter[i].innerText = counter[i].dataset.to;
-            clearInterval(int);
-          }
-        }, 60);
-      }
-    }
-  });
-
-})();
