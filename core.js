3d3787bc878f1d99dbe069ff9d8110c6336993cf
klaratokonova11 <148875652+Qwerty-pixl@users.noreply.github.com>
2024-03-25 14:10:38 +0600

    added some func but he again does not work

--- core.js
+++ core.js
@@ -29,7 +29,7 @@ function addCards(el) {
 <path d="M2 9.1371C2 14 6.01943 16.5914 8.96173 18.9109C10 19.7294 11 20.5 12 20.5C13 20.5 14 19.7294 15.0383 18.9109C17.9806 16.5914 22 14 22 9.1371C22 4.27416 16.4998 0.825464 12 5.50063C7.50016 0.825464 2 4.27416 2 9.1371Z" fill="#1C274C" class="qwer" id="btn"
 />
 </svg>
-      <a href="#openModal"><img src=${el.imges_2} alt=""></a>
+<a href="css3_modalwin/css3_modalwin/demo.html"><img src=${el.imges_2} alt=""></a>
       <div id="openModal" class="modal">
             <div class="modal-dialog">
               <div class="modal-content">
